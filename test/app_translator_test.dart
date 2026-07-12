0c634db29f2937f4d391b77e475e77fdff4e767f
g.m0h1 <144628717+mohamed09090-xmd@users.noreply.github.com>
2026-07-12 22:17:57 +0100

    Test French rendering and LTR direction

--- test/app_translator_test.dart
+++ test/app_translator_test.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/material.dart' as material;
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:game_credit_profit_manager/core/localization/app_translator.dart';
+import 'package:game_credit_profit_manager/core/localization/localized_text.dart'
+    as localized;
 
 void main() {
   test('يحافظ على النص العربي عند اختيار العربية', () {
@@ -36,6 +40,34 @@ void main() {
     expect(
       AppTranslator.translateForLanguage('من 01/07/2026 إلى 12/07/2026', 'fr'),
       'Du 01/07/2026 au 12/07/2026',
+    );
+  });
+
+  testWidgets('يعرض النص الفرنسي باتجاه LTR', (tester) async {
+    await tester.pumpWidget(
+      const material.MaterialApp(
+        locale: material.Locale('fr', 'FR'),
+        supportedLocales: [
+          material.Locale('ar', 'DZ'),
+          material.Locale('fr', 'FR'),
+        ],
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        home: material.Scaffold(
+          body: localized.Text('الإعدادات'),
+        ),
+      ),
+    );
+    await tester.pumpAndSettle();
+
+    expect(find.text('Paramètres'), findsOneWidget);
+    final element = tester.element(find.text('Paramètres'));
+    expect(
+      material.Directionality.of(element),
+      material.TextDirection.ltr,
     );
   });
 }
