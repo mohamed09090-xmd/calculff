7eccc569a5fe5e6b2a70e270b1f8392aa56abfc5
g.m0h1 <144628717+mohamed09090-xmd@users.noreply.github.com>
2026-07-12 21:56:05 +0100

    Add persistent Arabic and French language preference

---
+++ lib/shared/providers/app_language_provider.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+enum AppLanguagePreference { arabic, french }
+
+extension AppLanguagePreferenceX on AppLanguagePreference {
+  Locale get locale => switch (this) {
+        AppLanguagePreference.arabic => const Locale('ar', 'DZ'),
+        AppLanguagePreference.french => const Locale('fr', 'FR'),
+      };
+
+  TextDirection get textDirection => switch (this) {
+        AppLanguagePreference.arabic => TextDirection.rtl,
+        AppLanguagePreference.french => TextDirection.ltr,
+      };
+
+  String get languageCode => locale.languageCode;
+}
+
+class AppLanguageController extends AsyncNotifier<AppLanguagePreference> {
+  static const _storageKey = 'appearance.language.v1';
+  static const _storage = FlutterSecureStorage(
+    aOptions: AndroidOptions(migrateWithBackup: true),
+  );
+
+  @override
+  Future<AppLanguagePreference> build() async {
+    final stored = await _storage.read(key: _storageKey);
+    return AppLanguagePreference.values.firstWhere(
+      (language) => language.name == stored,
+      orElse: () => AppLanguagePreference.arabic,
+    );
+  }
+
+  Future<void> setLanguage(AppLanguagePreference language) async {
+    state = AsyncData(language);
+    await _storage.write(key: _storageKey, value: language.name);
+  }
+
+  Future<void> toggle() async {
+    final current = state.valueOrNull ?? AppLanguagePreference.arabic;
+    await setLanguage(
+      current == AppLanguagePreference.arabic
+          ? AppLanguagePreference.french
+          : AppLanguagePreference.arabic,
+    );
+  }
+}
+
+final appLanguageProvider =
+    AsyncNotifierProvider<AppLanguageController, AppLanguagePreference>(
+  AppLanguageController.new,
+);
