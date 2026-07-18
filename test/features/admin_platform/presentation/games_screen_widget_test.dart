7837feeda8c799b5c22f1238e3b40a0b44758401
g.m0h1 <144628717+mohamed09090-xmd@users.noreply.github.com>
2026-07-18 03:12:29 +0100

    style: format semantics assertion

--- test/features/admin_platform/presentation/games_screen_widget_test.dart
+++ test/features/admin_platform/presentation/games_screen_widget_test.dart
@@ -122,10 +122,7 @@ void main() {
 
     await _pumpGames(tester, repository: repository);
 
-    expect(
-      find.bySemanticsLabel(RegExp('تحديث الألعاب')),
-      findsWidgets,
-    );
+    expect(find.bySemanticsLabel(RegExp('تحديث الألعاب')), findsWidgets);
     expect(find.bySemanticsLabel(RegExp('إضافة لعبة')), findsWidgets);
     expect(
       find.bySemanticsLabel(RegExp('تعديل اللعبة فري فاير')),
